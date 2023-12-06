c4b060c12feb23be8c614da6c8890669e7a79eb9
vishaldev25 <vish2512d@gmail.com>
2023-12-06 10:20:26 +0000

    greetings card coding practice

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  <div className='bg-container'>
+    <h1>Congratulations</h1>
+    <div className='inner-container'>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png' />
+      <div className='text-container'>
+        <h2>Kiran V</h2>
+        <p>Vishnu Institute of Computer Education and Technology</p>
+        <p>Bheemavaram</p>
+      </div>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
